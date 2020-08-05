75b5994dc6d52e3446c655a98d3e1e81ce4ab695
smitteh1 <thejamesash1@gmail.com>
2020-08-04 21:47:52 -0400

    make reciever module

---
+++ HC12_Reciever/reciever/reciever.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial HC12(11,12);
+
+void setup()
+{
+	 Serial.begin(9600);
+   HC12.begin(9600);
+}
+
+void loop()
+{
+	while (HC12.available()) {
+    Serial.write(HC12.read());
+  }
+  while (Serial.available()) {
+    HC12.write(Serial.read());
+  }
+}
