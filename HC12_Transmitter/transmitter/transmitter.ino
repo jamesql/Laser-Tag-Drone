ee49c38e89ba187a3f79716c8c3e115a5ffa21b5
smitteh1 <thejamesash1@gmail.com>
2020-08-06 18:48:01 -0400

    add comments and increase seqMax

--- HC12_Transmitter/transmitter/transmitter.ino
+++ HC12_Transmitter/transmitter/transmitter.ino
@@ -1,27 +1,36 @@
 #include <SoftwareSerial.h>
 
+/* HC12 Object. */
 SoftwareSerial HC12(11,12);
 int ButtonPin = 13;
 
+/* Sequencing Variabels (kinda like aim assist). */
 int sequence = 0;
-int seqMax = 500;
+int seqMax = 2500;
 
+/* Constructor. */
 void setup()
 {
    HC12.begin(9600);
    pinMode(ButtonPin, INPUT);
 }
 
+/* Program Loop */
 void loop()
 {
+  /* Check if we are making a tasing attempt. */
   if (isGateOpened() || sequence > seqMax)
   {
-    HC12.write("Go Fuck Yourself\n");
+    /* Alert drone that someone needs to be tased. */
+    HC12.write("Tase Attempted.\n");
+    /* Reset Sequence */
     sequence = 0;
   }
+  /* Set to next sequence. */
   sequence++;
 }
 
+/* Check if button is pressed down. */
 bool isGateOpened()
 {
   
